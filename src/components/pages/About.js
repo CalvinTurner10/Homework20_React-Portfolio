95bc5132bc241d79d969cf70fb40791b5c3e9152
Calvin Turner <turnerjrcalvin@gmail.com>
2022-04-02 13:55:08 -0400

    about

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React from "react";
 
 export default function About() {
   return (
     <div>
-      <h1>About Page</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-5"></div>
+          <div className="col-xs-5">
+            <h1 className="about-header">About Me</h1>
+            <p>
+              Technical Support Engineer at a startup in Atlanta. His background
+              is providing outstanding customer support, great communication
+              skills, upgrading/configuring operating systems & antivirus
+              software programs. He is fueled by his passion for understanding
+              the nuances of new technology and software. Lorem ipsum dolor sit
+              amet consectetur adipisicing elit. Placeat voluptates numquam
+              rerum quidem nisi quia eligendi inventore obcaecati quasi, magnam
+              error aspernatur incidunt magni dicta dolor et? Deserunt, fugiat
+              soluta!
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
