2e669c2894cf961f5dfcf8fa28a18a3bf705b109
Calvin Turner <turnerjrcalvin@gmail.com>
2022-04-09 18:04:07 -0400

    contact

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,47 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 
-const ContactForm = () => {
-  const [status, setStatus] = useState("Submit");
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setStatus("Sending...");
-    const { name, email, message } = e.target.elements;
-    let details = {
-      name: name.value,
-      email: email.value,
-      message: message.value,
-    };
-    let response = await fetch("http://localhost:5000/contact", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=utf-8",
-      },
-      body: JSON.stringify(details),
-    });
-    setStatus("Submit");
-    let result = await response.json();
-    alert(result.status);
-  };
-  return (
-    <section id="contact">
-        <div className="container">
-          <div className="row">
-            <div className="col-12">
-              <h1 className="fw-bold text-center">Contact</h1>
-              <hr />
-            </div>
-          </div>
-          <div className="row mb-5">
-            <div className="col-md-6">
-              <a href="https://linkedin.com/in/calvin-t-320592167" className="link-dark">Linkedin</a>
-              <br />
-              <a href="turnerjrcalvin@gmail.com" className="link-dark">Email Me</a>
-              <br />
-              <a href="https://Github.com/CalvinTurner10" className="link-dark">Github</a>
-            </div></div></div></section>
-    );
-  }
+export default function Contacts () {
+return (
+  <div>
+    <div class = "middle">
+    <div> Github <a href="https://Github.com/CalvinTurner10" target="_blank"> Github</a></div>
+      <div> Linkedin <a href="https://linkedin.com/in/calvin-t-320592167" target="_blank"> Linkedin</a></div>
+      <div>Number: 678-899-9357</div>
+      <div> Email <a href="mailto:turnerjrcalvin@gmail.com" target="_blank"> Turnerjrcalvin@gmail.com</a></div>
 
-  
+    </div>
+  </div>
+);
 
-export default ContactForm;
+}
